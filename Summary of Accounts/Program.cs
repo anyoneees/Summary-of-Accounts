2ed0885345d8e9db25fc3bff1b82e265cdad3bab
anyonees <batarinau@mail.ru>
2022-05-04 17:58:43 +0300

    1

---
+++ Summary of Accounts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Summary_of_Accounts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
